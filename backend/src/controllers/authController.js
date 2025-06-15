2a91424d2be7cbe95ebbbd0a36eab8d6b81ad9a0
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2025-06-16 03:53:47 +0530

    onboarding profile update complete

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -104,3 +104,50 @@ export function logout(req, res) {
   res.status(200).json({ success: true, message: "Logout successful" });
 }
 
+export async function onboard(req,res){
+  try{
+    // console.log("hi");
+    const userId = req.user._id;
+   const {fullName,bio,nativeLanguage,learningLanguage,location}=req.body;
+     
+   if (!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ].filter(Boolean),
+      });
+    }
+  const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        ...req.body,
+        isOnboarded: true,
+      },
+      { new: true }
+    );
+
+    if (!updatedUser) return res.status(404).json({ message: "User not found" });
+
+    try {
+      await upsertStreamUser({
+        id: updatedUser._id.toString(),
+        name: updatedUser.fullName,
+        image: updatedUser.profilePic || "",
+      });
+      console.log(`Stream user updated after onboarding for ${updatedUser.fullName}`);
+    } catch (streamError) {
+      console.log("Error updating Stream user during onboarding:", streamError.message);
+    }
+
+    res.status(200).json({ success: true, user: updatedUser });
+
+      }catch(error){
+    console.log("Error in onboard controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+    }
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,17 @@
 import express  from "express";
-import { signup,login,logout } from "../controllers/authController.js";
+import { signup,login,logout, onboard } from "../controllers/authController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 
 router.post("/signup",signup);
 router.post("/Login",login);
-router.post("/Logout",logout);
+router.post("/Logout",logout);//Logout is a POST because it changes authentication state(state of server side ) and must be done intentionally and securely.
 
+
+router.post("/onboarding",protectRoute,onboard);
+//check if user logged in or not
+router.get("/me",protectRoute,(res,req)=>{
+    res.statusCode(200).json({success:true,user:req.user});
+})
 export default router;
