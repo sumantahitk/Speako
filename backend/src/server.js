363bb220d2d202d276777995ec8e15fc42dd732d
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2025-06-29 01:26:58 +0530

    env file update

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,27 +4,39 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
-const app= express()
-const PORT=process.env.PORT;
+const app = express()
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true //allow frontend to send cookies
+    origin: "http://localhost:5173",
+    credentials: true //allow frontend to send cookies
 }))
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/chat",chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.listen(PORT,()=>{
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    }
+    );
+
+}
+app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
     connectDB();
 })
